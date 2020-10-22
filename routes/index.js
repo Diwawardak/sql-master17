ac361d44b21d5901d478d86405c1099a7b3f8aaf
Diwa Wardak <diwawardak1@gmail.com>
2020-10-22 12:03:27 +0100

    hard coded updateName

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const routeController = require('../controllers/connectionController');
 routes.get('/', routeController.getIndex);
 routes.get('/StaffMember', routeController.getStaffMember);
 routes.post('/createStaffMember', routeController.postStaff);
+routes.patch('/updateStaffMember', routeController.updateStaffMember);
 routes.delete('/deleteStaffMember', routeController.deleteStaffMember);
+
 
 module.exports = routes; 
 
